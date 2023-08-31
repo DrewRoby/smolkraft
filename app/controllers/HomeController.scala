b5196c1b5c7b9fbd220423cd7b33b8e9d07ce2f7
Drew Roby (STL) <drew.roby@daugherty.com>
2023-08-30 22:29:05 -0500

    implement in-memory model

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,13 +1,34 @@
 package controllers
 
-import play.api.db._
+import models.{InMemoryModel, UserData}
+//import play.api.Database._
+import play.api.libs.json.{JsError, JsSuccess, Json, Reads}
 import play.api.mvc._
 
 import javax.inject._
 
 @Singleton
-class HomeController @Inject()(db: Database, val cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(val cc: ControllerComponents) extends AbstractController(cc) {
 
+  def withJsonBody[A](f: A => Result)(implicit request: Request[AnyContent], reads: Reads[A]) = {
+    request.body.asJson.map { body =>
+      Json.fromJson[A](body) match {
+        case JsSuccess(a, path) => f(a)
+        case e @ JsError(_) => Redirect(routes.HomeController.index())
+      }
+    }.getOrElse(Redirect(routes.HomeController.index()))
+  }
+
+  def validate = Action { implicit request =>
+    withJsonBody[UserData] { ud =>
+      if (InMemoryModel.validateUser(ud.username, ud.password)) {
+        Ok(Json.toJson(true))
+          .withSession("username" -> ud.username, "csrfToken" -> play.filters.csrf.CSRF.getToken.get.value)
+      } else {
+        Ok(Json.toJson(false))
+      }
+    }
+  }
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
--- app/models/InMemoryModel.scala
+++ app/models/InMemoryModel.scala
@@ -3,8 +3,8 @@ package models
 import scala.collection.mutable
 
 object InMemoryModel {
-  private val users = mutable.Map[String,String]("Mark" -> "pass")
-  private val tasks = mutable.Map[String, List[String]]("Mark" -> List("Make videos","eat","sleep","code"))
+  private val users = mutable.Map[String,String]("Drew" -> "pass")
+  private val items = mutable.Map[String, List[String]]("Drew" -> List("Kaolin Clay", "fragrance", "peppermint essential oil"))
 
   def validateUser(username: String, password: String): Boolean = {
     users.get(username).map(_ == password).getOrElse(false)
@@ -18,17 +18,17 @@ object InMemoryModel {
   }
 
   def getTasks(username: String): Seq[String] = {
-    tasks.get(username).getOrElse(Nil)
+    items.get(username).getOrElse(Nil)
   }
 
   def addTask(username: String, task: String): Unit = {
-    tasks(username) = task :: tasks.get(username).getOrElse(Nil)
+    items(username) = task :: items.get(username).getOrElse(Nil)
   }
 
   def removeTask(username: String, index: Int): Boolean = {
-    if (index < 0 || tasks.get(username).isEmpty || index >= tasks(username).length) false
+    if (index < 0 || items.get(username).isEmpty || index >= items(username).length) false
     else {
-      tasks(username) = tasks(username).patch(index, Nil, 1)
+      items(username) = items(username).patch(index, Nil, 1)
       true
     }
   }
