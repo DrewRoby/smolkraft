42e355eb47bff29bbd2d62332a6386b60eda1308
Roby <AJR1227@daugherty.com>
2025-03-04 21:33:40 -0600

    my take at 0011 migration

--- sk_backend/backend_api/views.py
+++ sk_backend/backend_api/views.py
@@ -1,59 +1,77 @@
-from django.shortcuts import render
-from backend_api.models import Vendor, OrderHeader, OrderLineItem, RawMaterial, RecipeHeader, RecipeAmount, RecipeItem
-from backend_api.serializers import VendorSerializer, OrderHeaderSerializer, \
-    OrderLineItemSerializer, RawMaterialSerializer, RecipeHeaderSerializer, \
-    RecipeAmountSerializer, RawMaterialNameSubSerializer
-from rest_framework import viewsets, status, filters, generics
-from rest_framework.decorators import api_view
-from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets, views, permissions
 from rest_framework.response import Response
+from .models import (
+    BOMHeader, BOMLine, BOOHeader, Operation, Material,
+    ProductionOrder, Inventory, OrderHeader, OrderLineItem
+)
+from .serializers import (
+    BOMDetailSerializer, ProductionOrderSerializer, ProductionOrderDetailSerializer,
+    InventorySerializer, CurrentInventorySerializer, ShortfallSerializer,
+    ProductionPossibilitySerializer, MaterialSerializer
+)
 
-# Create your views here.
-class VendorViewSet(viewsets.ModelViewSet):
-    queryset = Vendor.objects.all()
-    serializer_class = VendorSerializer
+# Basic viewsets for CRUD operations
+class MaterialViewSet(viewsets.ModelViewSet):
+    serializer_class = MaterialSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        return Material.objects.filter(insert_user=self.request.user)
 
-class OrderHeaderViewSet(viewsets.ModelViewSet):
-    queryset = OrderHeader.objects.all()
-    serializer_class = OrderHeaderSerializer
+class BOMViewSet(viewsets.ModelViewSet):
+    serializer_class = BOMDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        return BOMHeader.objects.filter(owner_user_id=self.request.user)
 
-class OrderLineItemViewSet(viewsets.ModelViewSet):
-    queryset = OrderLineItem.objects.all()
-    serializer_class = OrderLineItemSerializer
+class ProductionOrderViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductionOrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        return ProductionOrder.objects.filter(owner_user_id=self.request.user)
 
-class RawMaterialViewSet(viewsets.ModelViewSet):
-    queryset = RawMaterial.objects.all()
-    serializer_class = RawMaterialSerializer
-    search_fields = ['id', 'raw_material_short_name']
-    filter_backends = [filters.SearchFilter]
+    def retrieve(self, request, *args, **kwargs):
+        """Override to use detailed serializer for single item view"""
+        instance = self.get_object()
+        serializer = ProductionOrderDetailSerializer(instance)
+        return Response(serializer.data)
 
+class InventoryViewSet(viewsets.ModelViewSet):
+    serializer_class = InventorySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-class RecipeHeaderViewSet(viewsets.ModelViewSet):
-    queryset = RecipeHeader.objects.all()
-    serializer_class = RecipeHeaderSerializer
+    def get_queryset(self):
+        return Inventory.objects.filter(owner_user_id=self.request.user)
 
-#
-# class RecipeAmountListView(generics.ListCreateAPIView):
-#     queryset = RecipeAmount.objects.all()
-#     serializer_class = RecipeAmountSerializer
-#
-# class RecipeAmountDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = RecipeAmount.objects.all()
-#     serializer_class = RecipeAmountSerializer
+# Specialized views for complex data requirements
+class CurrentInventoryView(views.APIView):
+    """View for current inventory with lot numbers and expiration dates"""
+    permission_classes = [permissions.IsAuthenticated]
 
+    def get(self, request):
+        inventory = Inventory.objects.filter(
+            owner_user_id=request.user
+        ).select_related('material', 'production_order')
 
-class RecipeAmountViewSet(viewsets.ModelViewSet):
-    queryset = RecipeAmount.objects.all()
-    serializer_class = RecipeAmountSerializer
+        serializer = CurrentInventorySerializer(inventory, many=True)
+        return Response(serializer.data)
 
+class InventoryShortfallView(views.APIView):
+    """View for inventory shortfall analysis"""
+    permission_classes = [permissions.IsAuthenticated]
 
-# @login_required
-@api_view(['GET'])
-def myview(request):
-    # thing_in_question = RecipeItem.objects.all()
-    thing_in_question = RecipeAmount.objects.select_related()
-    serializer = RecipeAmountSerializer(thing_in_question, many=True)
-    return Response(serializer.data)
+    def get(self, request):
+        shortfall_data = ShortfallSerializer.get_shortfall_data(request.user.id)
+        serializer = ShortfallSerializer(shortfall_data, many=True)
+        return Response(serializer.data)
+
+class ProductionPossibilitiesView(views.APIView):
+    """View for production possibilities based on available inventory"""
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        possibilities = ProductionPossibilitySerializer.get_production_possibilities(request.user.id)
+        serializer = ProductionPossibilitySerializer(possibilities, many=True)
+        return Response(serializer.data)
