a81f9bcbe29943800a595bc7c6f5018a019eff60
Roby <AJR1227@daugherty.com>
2025-03-06 11:41:32 -0600

    point to new database

--- sk_backend/sk_backend/settings.py
+++ sk_backend/sk_backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +80,19 @@ WSGI_APPLICATION = 'sk_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'smolkraft_db',
+    #     'USER': 'root',
+    #     'PASSWORD': 'test'
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smolkraft_db',
-        'USER': 'root',
-        'PASSWORD': 'test'
+        'ENGINE': os.getenv("DB_ENGINE"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER_NAME"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
